e3496b388e8602e21104be9916004e232f9a0f6e
bbowhemian <bert@ntersol.com>
2022-08-01 10:49:56 -0500

    fixing more values into variables

--- aws/compute/ecs_cluster/ecs.tf
+++ aws/compute/ecs_cluster/ecs.tf
@@ -1,12 +1,12 @@
-resource "aws_ecs_cluster" "lead_api_ecs_cluster" {
-  name = "lead-api-ecs-cluster"
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.name}-ecs-cluster"
 
   configuration {
     execute_command_configuration {
       logging = "OVERRIDE"
       log_configuration {
         cloud_watch_encryption_enabled = false
-        cloud_watch_log_group_name     = "/ecs/leads-api-cluster"
+        cloud_watch_log_group_name     = "/ecs/${var.name}-cluster"
         s3_bucket_encryption_enabled   = false
                 }
     }
--- aws/compute/ecs_task_service/ecs-service.tf
+++ aws/compute/ecs_task_service/ecs-service.tf
@@ -1,24 +1,18 @@
-resource "aws_ecs_service" "lead_api_service" {
-  name                   = "lead-api-service"
-  cluster                = aws_ecs_cluster.lead_api_ecs_cluster.id
-  task_definition        = aws_ecs_task_definition.leads_api_task.arn
+resource "aws_ecs_service" "service" {
+  name                   = "${var.name}-service"
+  cluster                = var.cluster_id
+  task_definition        = aws_ecs_task_definition.task.arn
   desired_count          = 2
   launch_type            = "FARGATE"
   enable_execute_command = true
   load_balancer {
-    target_group_arn = aws_lb_target_group.lead_api_tg.arn
-    container_name   = "lead-api"
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = var.name
     container_port   = var.api_port
-  }
-
-  # load_balancer {
-  #   target_group_arn = aws_lb_target_group.lead_ui_tg.arn
-  #   container_name   = "lead-api"
-  #   container_port   = var.ui_port
-  # }  
+  } 
 
   network_configuration {
-    subnets         = [for subnet in aws_subnet.private : subnet.id]
-    security_groups = [aws_security_group.ecs_pub_sg.id]
+    subnets         = var.subnets
+    security_groups = var.security_groups
   }
 }
